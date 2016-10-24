e2a8961fa5438630235824b82d77673c7e860173
Sayem Hossain <sayemkcn@gmail.com>
2016-10-24 14:40:55 +0600

    completed update functionality for movie

--- src/main/java/net/toracode/moviedb/controllers/MovieController.java
+++ src/main/java/net/toracode/moviedb/controllers/MovieController.java
@@ -7,11 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import net.toracode.moviedb.entities.Movie;
 import net.toracode.moviedb.entities.Person;
@@ -22,23 +18,42 @@ import net.toracode.moviedb.services.PersonService;
 @Controller
 @RequestMapping(value = "/movie")
 public class MovieController {
-	@Autowired
-	private MovieService movieService;
-	@Autowired
-	private PersonService personService;
+    @Autowired
+    private MovieService movieService;
+    @Autowired
+    private PersonService personService;
 
-	@RequestMapping(value = "/create", method = RequestMethod.GET)
-	public String addMoviePage(Model model) {
-		model.addAttribute("personList",personService.findAll());
-		return "movie/create";
-	}
-	
-	@RequestMapping(value="/create",method=RequestMethod.POST)
-	public String addMovie(@ModelAttribute("movie") Movie movie,BindingResult bindingResult,@RequestParam("personIds") Long[] personIds){
-		List<Person> personList = personService.personListByIds(personIds);
-		movie.setCastAndCrewList(personList);
-		movie = movieService.save(movie);
-		System.out.println(movie.toString());
-		return "redirect:/movie/create?message=Successfully created movie!";
-	}
+    @RequestMapping(value = "/create", method = RequestMethod.GET)
+    public String addMoviePage(Model model) {
+        model.addAttribute("personList", this.personService.findAll());
+        return "movie/create";
+    }
+
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    public String addMovie(@ModelAttribute("movie") Movie movie, BindingResult bindingResult, @RequestParam("personIds") Long[] personIds) {
+        List<Person> personList = this.personService.personListByIds(personIds);
+        movie.setCastAndCrewList(personList);
+        movie = this.movieService.save(movie);
+        System.out.println(movie.toString());
+        return "redirect:/movie/create?message=Successfully created movie!";
+    }
+
+    @RequestMapping(value = "/update/{id}", method = RequestMethod.GET)
+    public String updateMoviePage(@PathVariable("id") Long id, Model model) {
+        Movie movie = this.movieService.getMovie(id);
+        model.addAttribute("movie", movie);
+        model.addAttribute("personList", this.personService.findAll());
+        return "movie/update";
+    }
+
+    @RequestMapping(value = "/update/{id}", method = RequestMethod.POST)
+    public String updateMovie(@ModelAttribute("movie") Movie movie, BindingResult bindingResult,@PathVariable("id") Long id,@RequestParam("personIds") Long[] personIds) {
+        if (bindingResult.hasErrors())
+            System.out.print(bindingResult.toString());
+        movie.setUniqueId(id);
+        List<Person> personList = this.personService.personListByIds(personIds);
+        movie.setCastAndCrewList(personList);
+        this.movieService.save(movie);
+		return "redirect:/movie/create?message=Successfully updated "+movie.getName();
+    }
 }
