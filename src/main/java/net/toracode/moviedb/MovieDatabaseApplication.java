ceb0d116010c9cbf113cb2f413e2c5545297f516
Sayem Hossain <sayemkcn@gmail.com>
2016-11-04 19:44:32 +0600

    war deployment for rhcloud

--- src/main/java/net/toracode/moviedb/MovieDatabaseApplication.java
+++ src/main/java/net/toracode/moviedb/MovieDatabaseApplication.java
@@ -2,11 +2,18 @@ package net.toracode.moviedb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
-public class MovieDatabaseApplication {
+public class MovieDatabaseApplication extends SpringBootServletInitializer{
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(MovieDatabaseApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieDatabaseApplication.class, args);
