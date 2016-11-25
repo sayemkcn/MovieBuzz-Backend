29ce8cf71d0c365190369570a20510d2dd00fe30
Sayem Hossain <sayemkcn@gmail.com>
2016-11-25 22:28:18 +0600

    added spring security

---
+++ src/main/java/net/toracode/moviedb/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package net.toracode.moviedb.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by sayemkcn on 11/25/16.
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/resources/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/", "/home", "/register", "/login", "/logout").permitAll()
+//                .antMatchers("/cv/**").hasAnyRole("ADMIN", "USER")
+                .anyRequest().authenticated();
+//                .and()
+//                .formLogin()
+//                .loginPage("/login")
+//                .loginProcessingUrl("/login")
+//                .failureUrl("/login?error")
+//                .permitAll();
+    }
+}
